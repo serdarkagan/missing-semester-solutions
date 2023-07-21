8c8502212d846f57fe765788041cae41082c8fbd
serdarkagan <serdarkagancelen@gmail.com>
2023-07-21 22:47:23 +0300

    update shell script

--- Lecture05/pidwait.sh
+++ Lecture05/pidwait.sh
@@ -2,11 +2,11 @@
 
 echo "Running program $0 with $# arguments with pid $$"
 n=0
-$(kill -0 $1) 
-while [ $? -eq 0 ]
+"kill -0 $1" 
+while $?;
 do
 sleep 1
-n=$(( $n + 1 ))
-$(kill -0 $1) 
+n=$(( n + 1 ))
+"kill -0 $1" 
 done
 echo "Sleeped $n seconds."
